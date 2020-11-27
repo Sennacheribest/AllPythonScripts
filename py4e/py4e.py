f18735f53288fc874eba310af0cf4404bf3bf695
Sennacheribest <36724340+Sennacheribest@users.noreply.github.com>
2020-11-27 23:23:38 +0800

    Create py4e.py

---
+++ py4e/py4e.py
@@ -0,0 +1,17 @@
+fname = input("Enter a file: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+fhand = open(fname)
+
+counts = dict()
+for line in fhand:
+    if line.startswith("From "): 
+        line = line.rstrip()
+        words = line.split()
+        pieces = words[5].split(":")[0]
+        counts[pieces] = counts.get(pieces, 0) + 1
+
+lst = list()       
+lst = sorted([ (key, value) for key, value in counts.items() ])
+
+for k, v in lst:
+    print(k, v)
